072012d605ead540171240b0ca78d8f4a615ed2f
Haochen Gou <hgou@ualberta.ca>
2023-03-21 15:34:50 -0600

    Update

--- App.js
+++ App.js
@@ -18,10 +18,14 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
