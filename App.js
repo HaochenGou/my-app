b21bcf281b2c5e09fc8bacbf905684c9d0006126
Haochen Gou <hgou@ualberta.ca>
2023-03-21 16:35:52 -0600

    Add button

--- App.js
+++ App.js
@@ -1,30 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Button from './components/Button';
+
+
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.footerContainer}>
-        <Button label="Input/Edit order" />
-        <Button label="Ready to deliver order" />
+        <Button theme="input" label="Input Order" />
+        <Button theme="view" label="View/Edit Order" />
+        <Button theme="paid" label="Paid Order" />
+        <Button theme="delivered" label="Delivered Order/History" />
       </View>
       <StatusBar style="auto" />
     </View>
-   
   );
 }
 
 const styles = StyleSheet.create({
-  footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  footerContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+
   }
 });
--- components/Button.js
+++ components/Button.js
@@ -1,12 +1,83 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function Button({ label }) {
-  return (
-    <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
-        <Text style={styles.buttonLabel}>{label}</Text>
+export default function Button({ label, theme }) {
+  if (theme === "input") {
+    return (
+      <View
+      style={[
+        styles.buttonContainer,
+        { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+      ]}>
+      <Pressable
+        style={[styles.button, { backgroundColor: '#fff' }]}
+        onPress={() => alert('You pressed a button.')}>
+        <Entypo name="add-to-list" size={24} color="black" style={styles.buttonIcon} />
+        <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
       </Pressable>
     </View>
+    );
+  }
+  else if (theme === "paid") {
+    return (
+      <View
+      style={[
+        styles.buttonContainer,
+        { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+      ]}>
+      <Pressable
+        style={[styles.button, { backgroundColor: '#fff' }]}
+        onPress={() => alert('You pressed a button.')}>
+        <MaterialIcons name="payments" size={24} color="black" style={styles.buttonIcon} />
+        <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+      </Pressable>
+    </View>
+    );
+  }
+
+  else if (theme === "view") {
+    return (
+      <View
+      style={[
+        styles.buttonContainer,
+        { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+      ]}>
+      <Pressable
+        style={[styles.button, { backgroundColor: '#fff' }]}
+        onPress={() => alert('You pressed a button.')}>
+       <Ionicons name="file-tray-full-outline" size={24} color="black" style={styles.buttonIcon} />
+        <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+      </Pressable>
+    </View>
+    );
+  }
+   else if (theme === "delivered") {
+    return (
+      <View
+      style={[
+        styles.buttonContainer,
+        { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+      ]}>
+      <Pressable
+        style={[styles.button, { backgroundColor: '#fff' }]}
+        onPress={() => alert('You pressed a button.')}>
+       <MaterialCommunityIcons name="truck-delivery" size={24} color="black" style={styles.buttonIcon} />
+        <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+      </Pressable>
+    </View>
+    );
+  }
+
+
+  return (
+    <View style={styles.buttonContainer}>
+        <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+          <Text style={styles.buttonLabel}>{label}</Text>
+        </Pressable>
+      </View>    
   );
 }
 
