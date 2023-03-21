5758e8977c67fb70d2a6c2df326330bcc54109f6
Haochen Gou <hgou@ualberta.ca>
2023-03-21 16:45:58 -0600

    UI

--- components/Button.js
+++ components/Button.js
@@ -86,6 +86,7 @@ const styles = StyleSheet.create({
     width: 320,
     height: 68,
     marginHorizontal: 20,
+    marginVertical: 10,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 3,
