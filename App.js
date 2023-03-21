5625a495d6b86192dcff3bfc418e037105849757
Haochen Gou <hgou@ualberta.ca>
2023-03-21 15:20:53 -0600

    Update

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Button from './components/Button';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello, World!</Text>
       <StatusBar style="auto" />
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
+   
   );
 }
 
