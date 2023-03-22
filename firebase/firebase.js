e385f71c564d8ed46887b22855dd422e4ace02c6
Haochen Gou <hgou@ualberta.ca>
2023-03-22 14:42:04 -0600

    Update

---
+++ firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjjF0yJ23Lqaa9FuuZJ_PvWJmBRwaRq2I",
+  authDomain: "hawke-inventory.firebaseapp.com",
+  projectId: "hawke-inventory",
+  storageBucket: "hawke-inventory.appspot.com",
+  messagingSenderId: "522626125556",
+  appId: "1:522626125556:web:221ee18da929cc1f5558a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
