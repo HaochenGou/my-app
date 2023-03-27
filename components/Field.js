9b8df75e6ac69ccf217513521a0c4d32cbd68d2b
Haochen Gou <hgou@ualberta.ca>
2023-03-27 09:02:05 -0600

    Update

--- components/Field.js
+++ components/Field.js
@@ -1,37 +1,38 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Pressable, Text, TextInput } from 'react-native';
 
+const Field = ({ label, inputType }) => {
+  const [input, setInput] = useState('');
 
-const Field = ({ label}) => {
-    const [input, setInput] = useState('');
+  const keyboardType = inputType === 'quantity' or ===''? 'numeric' : 'default';
 
-    
   return (
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>{label}</Text>
-        <TextInput
-          style={styles.input}
-          value={input}
-          onChangeText={setInput}
-        />
-      </View>
-    );
-
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType={keyboardType}
+        value={input}
+        onChangeText={setInput}
+      />
+    </View>
+  );
 };
+
 const styles = StyleSheet.create({
-    inputContainer: {
-      marginBottom: 10,
-    },
-    label: {
-      fontWeight: 'bold',
-      marginBottom: 5,
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: '#ccc',
-      padding: 10,
-      borderRadius: 5,
-    },
-  });
- 
+  inputContainer: {
+    marginBottom: 10,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
+
 export default Field;
--- pages/Input.js
+++ pages/Input.js
@@ -1,16 +1,94 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import Field from '../components/Field';
+import { app } from "../firebase/firebase";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 const InputPage = () => {
+  const [orderAddress, setOrderAddress] = useState('');
+  const [orderName, setOrderName] = useState('');
+  const [orderLicense, setOrderLicense] = useState('');
+  const [orderNumber, setOrderNumber] = useState('');
+  const [orderDate, setOrderDate] = useState('');
+  // Add alcohol quantities as state
+  const [birdieJuiceQuantity, setBirdieJuiceQuantity] = useState('');
+  const [babyXVodkaQuantity, setBabyXVodkaQuantity] = useState('');
+  // Add other alcohols here
+
+  const signIn = async (email, password) => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log("User signed in:", user);
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  };
+
+  const saveOrder = async () => {
+    try {
+      const ordersRef = collection(db, "Orders");
+      const docRef = await addDoc(ordersRef, {
+        orderAddress: orderAddress,
+        orderName: orderName,
+        orderLicense: orderLicense,
+        orderNumber: orderNumber,
+        orderDate: new Date(orderDate),
+      });
+      console.log("Order saved with ID:", docRef.id);
+
+      const alcoholOrders = [
+        { label: "Birdie Juice", quantity: birdieJuiceQuantity },
+        { label: "Baby-X-Vodka", quantity: babyXVodkaQuantity },
+        // Add other alcohols here
+      ];
+
+      // Save alcohol orders in the sub-collection
+      for (const alcoholOrder of alcoholOrders) {
+        const alcoholRef = collection(docRef, "Alcohol");
+        await addDoc(alcoholRef, {
+          label: alcoholOrder.label,
+          quantity: alcoholOrder.quantity,
+        });
+      }
+      console.log("Alcohol orders saved in the sub-collection.");
+    } catch (error) {
+      console.error("Error saving order:", error);
+    }
+  };
+
+  const cancelOrder = () => {
+    // Reset state values
+    setOrderAddress('');
+    setOrderName('');
+    setOrderLicense('');
+    setOrderNumber('');
+    setOrderDate('');
+    setBirdieJuiceQuantity('');
+    setBabyXVodkaQuantity('');
+    // Reset other alcohol quantities
+  };
+
+  signIn("haochen@hawkepro.com", "hawkeprohibition");
 
   return (
     <View style={styles.container}>
-        <Field label="Order Address"/>
-        <Field label="Order Name"/>
-        <Field label="Order License"/>
-        <Field label="Order Payment"/>
-        <Field label="Order number"/>
+      <Field label="Order Address" onChangeText={setOrderAddress} />
+      <Field label="Order Name" onChangeText={setOrderName} />
+      <Field label="Order License" onChangeText={setOrderLicense} />
+      <Field label="Order number" onChangeText={setOrderNumber} />
+      <Field label="Order Date" onChangeText={setOrderDate} isDateInput />
+      <Field label="Birdie Juice" onChangeText={setBirdieJuiceQuantity} />
+      <Field label="Baby-X-Vodka" onChangeText={setBabyXVodkaQuantity} />
+      
+      {/* Add other alcohol fields here */}
+      {/* Add more alcohol fields as needed */}
+      <Button title="Save" onPress={saveOrder} />
+      <Button title="Cancel" onPress={cancelOrder} />
     </View>
   );
 };
@@ -23,3 +101,4 @@ const styles = StyleSheet.create({
 });
 
 export default InputPage;
+
